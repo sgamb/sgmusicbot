11e651c4e05f21f5c9f4876fbd84479a933e16e3
Sergey Gambaryan <sgamb2000@gmail.com>
2022-01-03 22:28:13 +0300

    Override upload script to command line utilite

--- source/upload.py
+++ source/upload.py
@@ -1,56 +1,37 @@
+import argparse
 import logging
 import os
 from pathlib import Path
 
 from dotenv import load_dotenv
-from sqlalchemy.orm import Session
-
 from sending import CHAT_ID, bot
 from shelf import Record, Track, engine
+from sqlalchemy.orm import Session
 
 load_dotenv()
-HOME_DIR = os.getenv('HOME_DIR')
+
+parser = argparse.ArgumentParser(description='upload files to telegram server')
+parser.add_argument('path', type=Path, help='directory to upload', nargs='*')
 
 
-def read_home_dir():
-    """ Read the file system and record every album from each year
-
-    ├── 1965
-    │   └── The Beatles - Rubber Soul
-    ├── 1966
-    │   ├── Bob Dylan - Blonde On Blonde
-    │   ├── Fresh Cream - Cream
-    │   └── ...
-    └── ...
-
-    """
-    home_dir = Path(HOME_DIR)
-    years = os.listdir(home_dir)
-    session = Session(engine)
-#   Iterating over years, keep year variable
-    for year in sorted(years):
-        year_dir = home_dir / year
-        albums = os.listdir(year_dir)
-    #   Iterating over albums, keep album variable
-        for album in sorted(albums):
-            album_dir = year_dir / album
-        #   New Record instance
-            disk = Record(year=year, record_name=album)
-            session.add(disk)
-        #   Beggining of track recording
-        #   from home_dir/year_dir/album_dir Path
-            record_tracks_of(disk, album_dir, session)
-            session.commit()
-    session.close()
+def record_disk(album_dir):
+    """ Create new DISK and start track recording """
+    year = os.path.basename(os.path.dirname(album_dir))
+    album = os.path.basename(album_dir)
+    with Session(engine) as session:
+        disk = Record(year=year, record_name=album)
+        session.add(disk)
+        record_tracks_of(disk, album_dir, session)
+        session.commit()
 
 
 def record_tracks_of(record, album_dir, session):
     """ Create new Track instance for each sended track """
+    bot.send_message(chat_id=CHAT_ID, text=record.record_name)
     tracks = get_tracks(album_dir)
     for track_name in tracks:
         mp3 = album_dir / track_name
         file_id = send_file(mp3)
-    #   New Track instance
         track = Track(
             record_id=record.id,
             track_name=track_name,
@@ -81,9 +62,6 @@ def send_file(mp3):
 
 
 if __name__ == '__main__':
-    print('Are you shure you want to start music collecting? [y/n]: ', end='')
-    answer = input()
-    if answer in 'yY':
-        read_home_dir()
-    else:
-        print('Canceled')
+    args = parser.parse_args()
+    for path in args.path:
+        record_disk(path)
