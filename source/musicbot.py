d43d7851b7a2a338df925987be7db6028f29d0bf
Sergey Gambaryan <sgamb2000@gmail.com>
2022-03-14 00:28:26 +0300

    Refactor code in my new IDE

--- source/musicbot.py
+++ source/musicbot.py
@@ -4,36 +4,34 @@ import random
 from dotenv import load_dotenv
 from telegram.ext import CommandHandler, MessageHandler, Updater
 
-from filters import record_id_filter
-from sending import send_info, send_record
+import filters
+import sending
 import shelf
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
-
-updater = Updater(token=TOKEN, use_context=True)
+updater = Updater(token=os.getenv('TOKEN'))
 dispatcher = updater.dispatcher
 
 
-def handle_record_id(update, context):
+def handle_record_id(update):
     record_id = update.message.text
-    send_record(record_id, update.message.from_user.id)
+    sending.send_record(record_id, update.message.from_user.id)
 
 
 def handle_start(update, context):
-    send_info('hi', update.message.from_user.id)
+    context.bot.send_message(text='hi', chat_id=update.message.from_user.id)
 
 
-def handle_random(update, context):
+def handle_random(update):
     number_of_records = shelf.count_records()
     random_record_id = random.randint(1, number_of_records)
-    send_record(random_record_id, update.message.from_user.id)
+    sending.send_record(random_record_id, update.message.from_user.id)
 
 
+record_id_handler = MessageHandler(filters.record_id_filter, handle_record_id)
 start_handler = CommandHandler('start', handle_start)
 random_handler = CommandHandler('random', handle_random)
-record_id_handler = MessageHandler(record_id_filter, handle_record_id)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(random_handler)
--- source/sending.py
+++ source/sending.py
@@ -2,28 +2,18 @@ import logging
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, select
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from telegram import Bot
 
-from shelf import Track
+from shelf import engine, Track
+
 
 load_dotenv()
-
-TOKEN = os.getenv('TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
-DB_URL = os.getenv('DB_URL')
-
-engine = create_engine(DB_URL, echo=True, future=True)
-
-bot = Bot(token=TOKEN)
+bot = Bot(token=os.getenv('TOKEN'))
 
 
-def send_info(text, chat_id=CHAT_ID):
-    bot.send_message(text=text, chat_id=chat_id)
-
-
-def send_track(file_id, chat_id=CHAT_ID):
+def send_track(file_id, chat_id):
     """ Sends one track by its file_id """
     try:
         bot.send_audio(chat_id, audio=file_id)
@@ -31,7 +21,7 @@ def send_track(file_id, chat_id=CHAT_ID):
         logging.error(err)
 
 
-def send_record(record_id, chat_id=CHAT_ID):
+def send_record(record_id, chat_id):
     """ Sends all tracks from album by given record_id """
     with Session(engine) as session:
         stmt = select(Track.file_id).where(Track.record_id == record_id)
