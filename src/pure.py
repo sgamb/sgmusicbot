2c7f8a1c3eaec4ca4e4be23f210963c4dd5995f2
Sergey Gambaryan <sgamb2000@gmail.com>
2022-09-17 07:13:45 +0300

    Handle an update

--- src/pure.py
+++ src/pure.py
@@ -8,7 +8,14 @@ async def main():
     load_dotenv()
     bot = telegram.Bot(os.getenv('TOKEN'))
     async with bot:
-        await bot.send_message(text='Hello, John', chat_id=144407396)
+        # await bot.send_message(text='Hello, John', chat_id=144407396)
+
+        print((await bot.getUpdates(
+            offset=77824976,
+            limit=1,
+            timeout=3,
+            allowed_updates=["message"],
+        ))[0])
 
 
 if __name__ == '__main__':
