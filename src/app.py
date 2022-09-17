1bdf3aa40908865995f3285d5c91e73a34cee09b
Sergey Gambaryan <sgamb2000@gmail.com>
2022-09-17 12:19:24 +0300

    Refactor and add other possibilities

--- src/app.py
+++ src/app.py
@@ -3,7 +3,8 @@ import random
 import logging
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes
+from telegram.ext import CommandHandler as ch
 
 from main import get_track_file_ids
 
@@ -12,24 +13,33 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+
 async def lucky(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
     albumID = random.randint(1, 100)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=str(albumID))
-    trackFileIDs = get_track_file_ids(albumID)
-    for fileID in trackFileIDs:
-        await context.bot.send_audio(chat_id=update.effective_chat.id, audio=fileID)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="You are lucky today :)")
+
+    if albumID <= 35:
+        trackFileIDs = get_track_file_ids(albumID)
+        for fileID in trackFileIDs:
+            await context.bot.send_audio(chat_id=update.effective_chat.id, audio=fileID)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="You are lucky today :)")
+    else:
+        await context.bot_send_message(chat_id=update.effective_chat.id, text="Sorry, not lucky :(")
 
 if __name__ == '__main__':
     load_dotenv()
-    application = ApplicationBuilder().token(os.getenv('TOKEN')).build()
+
+    app = ApplicationBuilder().token(os.getenv('TOKEN')).build()
     
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    lucky_handler = CommandHandler('lucky', lucky)
-    application.add_handler(lucky_handler)
+    start_handler = ch('start', start)
+    app.add_handler(start_handler)
+
+    lucky_handler = ch('lucky', lucky)
+    app.add_handler(lucky_handler)
     
-    application.run_polling()
+    app.run_polling()
