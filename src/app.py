ac7cf2890f427002214e90cd96470b99549457cd
Sergey Gambaryan <sgamb2000@gmail.com>
2023-03-15 20:54:16 +0300

    Add direct album support

--- src/app.py
+++ src/app.py
@@ -8,10 +8,10 @@ from telegram.ext import CommandHandler as ch
 
 from main import get_track_file_ids
 
-# logging.basicConfig(
-#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-#     level=logging.INFO
-# )
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -30,7 +30,7 @@ async def lucky(update: Update, context: ContextTypes.DEFAULT_TYPE):
             text=str(albumID),
     )
 
-    if albumID <= 35:
+    if 35 < albumID <= 65:
         trackFileIDs = get_track_file_ids(albumID)
         await context.bot.send_message(
                 chat_id=update.effective_chat.id,
@@ -51,9 +51,36 @@ async def lucky(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 text="Sorry, no luck :(",
         )
 
+
+async def album(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        albumID = int(update.message.text.split()[-1])
+    except:
+        await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="Usage: /album 13",
+        )
+        return
+    trackFileIDs = get_track_file_ids(albumID)
+    await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="OK, sending audio",
+    )
+    for fileID in trackFileIDs:
+        await context.bot.send_audio(
+                chat_id=update.effective_chat.id,
+                audio=fileID,
+        )
+    await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Done",
+    )
+
 if __name__ == '__main__':
+    print('starting')
     load_dotenv()
     app = ApplicationBuilder().token(os.getenv('TOKEN')).build()
     app.add_handler(ch('start', start))
     app.add_handler(ch('lucky', lucky))
+    app.add_handler(ch('album', album))
     app.run_polling()
